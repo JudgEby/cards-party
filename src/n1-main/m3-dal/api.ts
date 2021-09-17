306f5d8eeef835a75586e4d7b1b732f37c22ca6b
yan1122 <yan.tiger8@gmail.com>
2021-09-17 21:03:10 +0300

    created instanse

--- src/n1-main/m3-dal/api.ts
+++ src/n1-main/m3-dal/api.ts
@@ -1,1 +1,10 @@
-export const a = () => {}
+import axios from 'axios'
+
+const instance = axios.create({
+	baseURL: 'https://social-network.samuraijs.com/api/1.1/',
+	withCredentials: true
+})
+
+export const authAPI = () => {
+
+}
