a74cffb1d64826d44bc8e5dd91caf1bcf8252aa7
Arkady Polhovsky <arkims@inbox.ru>
2021-09-18 02:27:52 +0300

    create functions for registration page

--- src/n2-features/f2-registration/Registration.tsx
+++ src/n2-features/f2-registration/Registration.tsx
@@ -1,24 +1,43 @@
-import React, { useState } from 'react'
+import React, { ChangeEvent, useState } from 'react'
 import SuperInputText from '../../n1-main/m1-ui/common/SuperInputText/SuperInputText'
 import SuperButton from '../../n1-main/m1-ui/common/SuperButton/SuperButton'
 import s from './Registration.module.css'
+import { useHistory } from 'react-router-dom'
 
 const Registration = () => {
 	const [login, setLogin] = useState<string>('')
 	const [password, setPassword] = useState<string>('')
 	const [checkPassword, setCheckPassword] = useState<string>('')
 
+	const history = useHistory()
+
+	const onChangePassword = (e: ChangeEvent<HTMLInputElement>) => {
+		setPassword(e.currentTarget.value)
+	}
+	const onChangeCheckPassword = (e: ChangeEvent<HTMLInputElement>) => {
+		setCheckPassword(e.currentTarget.value)
+	}
+
+	const abortRegistration = () => {
+		history.push('/login')
+	}
+
+	const handleSubmit = () => {
+		if (password === checkPassword) {
+			history.push('/login')
+		}
+	}
+
 	return (
 		<div>
 			<h4>Sign Up</h4>
-			<form>
+			<form onSubmit={handleSubmit}>
 				<div><SuperInputText placeholder='email' value={login} onChangeText={setLogin} /></div>
-				<div><input type='password' placeholder='password' value={password}
-								onChange={(e) => setPassword(e.currentTarget.value)} /></div>
+				<div><input type='password' placeholder='password' value={password} onChange={onChangePassword} /></div>
 				<div><input type='password' placeholder='confirm password' value={checkPassword}
-								onChange={(e) => setCheckPassword(e.currentTarget.value)} /></div>
+								onChange={onChangeCheckPassword} /></div>
 				<div>
-					<SuperButton className={s.regBtn}>Cancel</SuperButton>
+					<SuperButton className={s.regBtn} onClick={abortRegistration}>Cancel</SuperButton>
 					<SuperButton className={s.regBtn} type='submit'>Register</SuperButton>
 				</div>
 			</form>
