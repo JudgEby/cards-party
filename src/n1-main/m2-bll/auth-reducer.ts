453112ca480f4bc416ab3384eaf572abf44832bd
yan1122 <yan.tiger8@gmail.com>
2021-09-17 23:01:51 +0300

    LoginPage

--- src/n1-main/m2-bll/auth-reducer.ts
+++ src/n1-main/m2-bll/auth-reducer.ts
@@ -1,25 +1,77 @@
 import { AppThunk } from './store'
+import { authAPI } from '../m3-dal/api'
 
-const initialState = {}
-type InitialStateType = typeof initialState
+const initialState = {
+	_id: null,
+	isAuthorized: false,
+	email: null,
+	name: null,
+	avatar: null,
+	publicCardPacksCount: 0,
+	verified: false
+}
+type InitialStateType = {
+	_id: null | string,
+	isAuthorized: boolean,
+	email: null | string,
+	name: null | string,
+	avatar: null | string,
+	publicCardPacksCount: number,
+	verified: boolean
+}
 
-export const authReducer = (
-	state: InitialStateType = initialState,
-	action: AuthActionsType
-): InitialStateType => {
+export const authReducer = (state: InitialStateType = initialState, action: AuthActionsType): InitialStateType => {
 	switch (action.type) {
-		case 'TEST':
-			return state
+		case 'AUTH/LOGIN':
+			return {
+				...state,
+				isAuthorized: action.data.isAuthorized,
+				_id: action.data._id,
+				avatar: action.data.avatar,
+				name:action.data.name,
+				email:action.data.email,
+				publicCardPacksCount:action.data.publicCardPacksCount,
+				verified:action.data.verified
+			}
 		default:
 			return state
 	}
 }
 // actions
-const test = () => ({ type: 'TEST' } as const)
-// thunks
-export const testTC = (): AppThunk => dispatch => {
-	dispatch(test())
-}
-// types
+const Login = (data: dataType) =>
+	({ type: 'AUTH/LOGIN', data } as const)
 
-export type AuthActionsType = ReturnType<typeof test>
+// thunks
+export const LoginTC = (loginData:loginDataType): AppThunk => dispatch => {
+	authAPI.login(loginData).then(res => {
+		let data:dataType = {
+			_id:res.data._id,
+			avatar:res.data.avatar,
+			name:res.data.name,
+			email:res.data.email,
+			publicCardPacksCount:res.data.publicCardPacksCount,
+			verified:res.data.verified,
+			isAuthorized:true
+		}
+		dispatch(Login(data))
+	})
+
+}
+
+// types
+export type dataType = {
+	_id: string,
+	isAuthorized: boolean,
+	email: string,
+	name: string,
+	avatar: string,
+	publicCardPacksCount: number,
+	verified: boolean
+}
+export type loginDataType = {
+	email:string,
+	password:string,
+	rememberMe:boolean
+}
+
+export type AuthActionsType = ReturnType<typeof Login>
--- src/n1-main/m3-dal/api.ts
+++ src/n1-main/m3-dal/api.ts
@@ -1,10 +1,13 @@
 import axios from 'axios'
+import { loginDataType } from '../m2-bll/auth-reducer'
 
 const instance = axios.create({
 	baseURL: 'http://localhost:7542/2.0/',
 	withCredentials: true
 })
 
-export const authAPI = () => {
-
+export const authAPI = {
+login(loginData:loginDataType){
+ return instance.post('/auth/login',loginData)
+	}
 }
