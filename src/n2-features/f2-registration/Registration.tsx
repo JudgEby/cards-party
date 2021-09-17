1f9a8c42d76e88f9c54ca71978e836f731e09339
Arkady Polhovsky <arkims@inbox.ru>
2021-09-18 01:38:44 +0300

    render registration page

--- src/n2-features/f2-registration/Registration.tsx
+++ src/n2-features/f2-registration/Registration.tsx
@@ -1,7 +1,23 @@
 import React from 'react'
+import SuperInputText from '../../n1-main/m1-ui/common/SuperInputText/SuperInputText'
+import SuperButton from '../../n1-main/m1-ui/common/SuperButton/SuperButton'
+import s from './Registration.module.css'
 
 const Registration = () => {
-	return <div>Registration</div>
+	return (
+		<div>
+			<h4>Sign Up</h4>
+			<form>
+				<div><SuperInputText /></div>
+				<div><input type='password' /></div>
+				<div><input type='password' /></div>
+				<div>
+					<SuperButton className={s.regBtn}>Cancel</SuperButton>
+					<SuperButton className={s.regBtn} type='submit'>Register</SuperButton>
+				</div>
+			</form>
+		</div>
+	)
 }
 
 export default Registration
