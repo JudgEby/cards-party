18e5b210f4eaf9b5b8d115816a36a72af27b26fd
Arkady Polhovsky <arkims@inbox.ru>
2021-09-18 02:00:40 +0300

    create state for registration page

--- src/n2-features/f2-registration/Registration.tsx
+++ src/n2-features/f2-registration/Registration.tsx
@@ -1,16 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import SuperInputText from '../../n1-main/m1-ui/common/SuperInputText/SuperInputText'
 import SuperButton from '../../n1-main/m1-ui/common/SuperButton/SuperButton'
 import s from './Registration.module.css'
 
 const Registration = () => {
+	const [login, setLogin] = useState<string>('')
+	const [password, setPassword] = useState<string>('')
+	const [checkPassword, setCheckPassword] = useState<string>('')
+
 	return (
 		<div>
 			<h4>Sign Up</h4>
 			<form>
-				<div><SuperInputText /></div>
-				<div><input type='password' /></div>
-				<div><input type='password' /></div>
+				<div><SuperInputText placeholder='email' value={login} onChangeText={setLogin} /></div>
+				<div><input type='password' placeholder='password' value={password}
+								onChange={(e) => setPassword(e.currentTarget.value)} /></div>
+				<div><input type='password' placeholder='confirm password' value={checkPassword}
+								onChange={(e) => setCheckPassword(e.currentTarget.value)} /></div>
 				<div>
 					<SuperButton className={s.regBtn}>Cancel</SuperButton>
 					<SuperButton className={s.regBtn} type='submit'>Register</SuperButton>
