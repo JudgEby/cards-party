3942863140a0dbb316cdbe3fe1725e0f1b37b648
yan1122 <yan.tiger8@gmail.com>
2021-09-18 12:38:50 +0300

    auth me

--- src/n1-main/m3-dal/api.ts
+++ src/n1-main/m3-dal/api.ts
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import { loginDataType } from '../m2-bll/auth-reducer'
 
 const instance = axios.create({
 	baseURL: 'http://localhost:7542/2.0/',
@@ -8,7 +9,7 @@ const instance = axios.create({
 export const authAPI = {
 	login(loginData:loginDataType){
 		return instance.post('/auth/login',loginData)
-	}
+	},
 	getMe() {
 		return instance.post('auth/me', {})
 	},
