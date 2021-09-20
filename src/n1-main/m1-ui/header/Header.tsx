07879157ef4fa01a6bb564c483dd99dfe8239604
Alex Samal <70150329+Neulovimij@users.noreply.github.com>
2021-09-20 19:02:03 +0300

    css for menu(Header) add link hover

--- src/n1-main/m1-ui/header/Header.tsx
+++ src/n1-main/m1-ui/header/Header.tsx
@@ -17,18 +17,18 @@ const Header = () => {
 	}
 	return (
 		<header className={styles.header}>
-			<NavLink to={'/'} className={styles.link}>Home</NavLink>
+			<NavLink to={'/'} className={styles.link} activeClassName={styles.active}>Home</NavLink>
 			{isAuthorized ? (
 				<button onClick={logoutHandler}>Logout</button>
 			) : (
-				<NavLink to={'/login'} className={styles.link}>Login</NavLink>
+				<NavLink to={'/login'} className={styles.link} activeClassName={styles.active}>Login</NavLink>
 			)}
-			<NavLink to={'/registration'} className={styles.link}>Registration</NavLink>
-			<NavLink to={'/profile'} className={styles.link}>Profile</NavLink>
-			<NavLink to={'/404'} className={styles.link}>404</NavLink>
-			<NavLink to={'/password/recovery'} className={styles.link}>Password Recovery</NavLink>
-			<NavLink to={'/password/new'} className={styles.link}>New Password</NavLink>
-			<NavLink to={'/test'} className={styles.link}>Test</NavLink>
+			<NavLink to={'/registration'} className={styles.link} activeClassName={styles.active}>Registration</NavLink>
+			<NavLink to={'/profile'} className={styles.link} activeClassName={styles.active}>Profile</NavLink>
+			<NavLink to={'/404'} className={styles.link} activeClassName={styles.active}>404</NavLink>
+			<NavLink to={'/password/recovery'} className={styles.link} activeClassName={styles.active}>Password Recovery</NavLink>
+			<NavLink to={'/password/new'} className={styles.link} activeClassName={styles.active}>New Password</NavLink>
+			<NavLink to={'/test'} className={styles.link} activeClassName={styles.active}>Test</NavLink>
 			<div><img src={menu} className={styles.block} alt={menu}/></div>
 		</header>
 	)
