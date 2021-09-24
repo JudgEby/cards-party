0ef12d7ffaab81dd75f2efb9fa6708a83ec69447
Yan Dobrovolskiy <bloodwoodwood@gmail.com>
2021-09-24 17:55:45 +0300

    change back api to local links

--- src/n1-main/m3-dal/api.ts
+++ src/n1-main/m3-dal/api.ts
@@ -1,12 +1,12 @@
 import axios from 'axios'
 import { loginDataType } from '../m2-bll/auth-reducer'
 
-const instance2 = axios.create({
+const instance = axios.create({
 	baseURL: 'http://localhost:7542/2.0/',
 	withCredentials: true,
 })
 
-const instance = axios.create({
+const instance2 = axios.create({
 	baseURL: 'https://neko-back.herokuapp.com/2.0/',
 	withCredentials: true,
 })
@@ -35,7 +35,7 @@ export const authAPI = {
 			{
 				email,
 				from: 'Best of the best',
-				message: `<div style='background-color: lime; padding: 15px'>password recovery link: <a href='${linkInRecoverEmailToGithubPages}$token$'>link</a></div>`,
+				message: `<div style='background-color: lime; padding: 15px'>password recovery link: <a href=${linkInRecoverEmailToLocal}>link</a></div>`,
 			},
 			{ withCredentials: true }
 		)
