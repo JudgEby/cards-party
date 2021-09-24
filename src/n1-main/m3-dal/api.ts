d3b29e8bf24485c2257fe83564e2fee11134b268
Yan Dobrovolskiy <bloodwoodwood@gmail.com>
2021-09-24 17:24:46 +0300

    change api instance to https://neko-back.herokuapp.com/2.0 for deploy to githubpages

--- src/n1-main/m3-dal/api.ts
+++ src/n1-main/m3-dal/api.ts
@@ -1,8 +1,13 @@
 import axios from 'axios'
 import { loginDataType } from '../m2-bll/auth-reducer'
 
-const instance = axios.create({
+const instance2 = axios.create({
 	baseURL: 'http://localhost:7542/2.0/',
+	withCredentials: true,
+})
+
+const instance = axios.create({
+	baseURL: 'https://neko-back.herokuapp.com/2.0/',
 	withCredentials: true,
 })
 
