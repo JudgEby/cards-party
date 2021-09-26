a21ce8b0be4870f5667e4b4d3dfeb8b8b2f3450c
Yan Dobrovolskiy <bloodwoodwood@gmail.com>
2021-09-26 18:36:04 +0300

    beautified Profile

--- src/n2-features/f3-profile/Profile.tsx
+++ src/n2-features/f3-profile/Profile.tsx
@@ -9,6 +9,7 @@ import { AppRootStateType } from '../../n1-main/m2-bll/store'
 import styles from './Profile.module.css'
 import { Redirect } from 'react-router-dom'
 import defaultAvatar from '../../Media/avatar/icon-avatar.jpg'
+import editIcon from '../../Media/edit-icon.svg'
 
 const Profile = () => {
 	const dispatch = useDispatch()
@@ -20,10 +21,33 @@ const Profile = () => {
 		state => state.auth.isAuthorized
 	)
 	const [newAvatarInputValue, setNewAvatarInputValue] = useState('')
+	const [editAvatarMode, setEditAvatarMode] = useState(false)
 	const [newNameInputValue, setNewNameInputValue] = useState('')
+	const [editNameMode, setEditNameMode] = useState(false)
 
 	if (!isAuthorized) {
 		return <Redirect to={'/login'} />
+	}
+
+	const editAvatar = () => {
+		if (!editAvatarMode) {
+			setEditAvatarMode(true)
+		} else {
+			setEditAvatarMode(false)
+			setNewAvatarInputValue('')
+		}
+	}
+
+	const editName = () => {
+		if (!editNameMode) {
+			setEditNameMode(true)
+			if (name) {
+				setNewNameInputValue(name)
+			}
+		} else {
+			setEditNameMode(false)
+			setNewNameInputValue('')
+		}
 	}
 
 	const sendNewAvatar = (e: KeyboardEvent<HTMLInputElement>) => {
@@ -41,7 +65,7 @@ const Profile = () => {
 	}
 
 	return (
-		<div>
+		<div className={styles.profileContainer}>
 			<div
 				className={styles.avatar}
 				style={{
@@ -49,24 +73,54 @@ const Profile = () => {
 						avatar || defaultAvatar
 					}) no-repeat center/cover`,
 				}}
-			/>
+			>
+				<div
+					className={styles.avatarChangeIconWrapper}
+					onClick={editAvatar}
+				>
+					<div
+						style={{
+							background: `url(${editIcon}) no-repeat center/cover`,
+						}}
+						className={styles.avatarChangeIcon}
+					/>
+				</div>
+			</div>
 			<input
+				style={editAvatarMode ? {} : { display: 'none' }}
 				type='text'
 				placeholder={'enter new avatar URL'}
 				value={newAvatarInputValue}
 				onChange={e => setNewAvatarInputValue(e.target.value)}
-				onBlur={() => setNewAvatarInputValue('')}
+				onBlur={() => {
+					setNewAvatarInputValue('')
+					editAvatar()
+				}}
 				onKeyDown={sendNewAvatar}
 			/>
-			<div>{name}</div>
+			<div className={styles.userNameWrapper}>
+				<span className={styles.name}>{name}</span>
+				<span
+					style={{
+						background: `url(${editIcon}) no-repeat center/cover`,
+					}}
+					className={styles.avatarChangeIcon}
+					onClick={editName}
+				/>
+			</div>
 			<input
+				style={editNameMode ? {} : { display: 'none' }}
 				type='text'
 				placeholder={'enter new name'}
 				value={newNameInputValue}
 				onChange={e => setNewNameInputValue(e.target.value)}
-				onBlur={() => setNewNameInputValue('')}
+				onBlur={() => {
+					setNewNameInputValue('')
+					editName()
+				}}
 				onKeyDown={sendNewName}
 			/>
+
 			<div>{`email: ${email}`}</div>
 			<div>{`Количество колод: ${publicCardPacksCount}`}</div>
 			<div>{`Почта подтверждена: ${verified ? 'Да' : 'Нет'}`}</div>
